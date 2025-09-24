55fd7689213ffe470a7c0edae9fb2b29aa4388e9
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2025-09-24 13:19:24 +0100

    Added keys listing function

--- src/index.ts
+++ src/index.ts
@@ -36,6 +36,15 @@ export async function remove(key: string): Promise<void> {
     await fs.unlink(path.join(process.cwd(), `/.kv/${key}.cbor`));
 }
 
+export async function keys(): Promise<string[]> {
+    try {
+        const files = await fs.readdir(path.join(process.cwd(), "/.kv"));
+        return files.map(file => file.replace(".cbor", ""));
+    } catch {
+        return [];
+    }
+}
+
 async function createDirectoryIfAbsent(): Promise<void> {
     const directory = path.join(process.cwd(), "/.kv");
 
