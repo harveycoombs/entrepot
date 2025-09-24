454200925ebe8b3466dc41a5a0975a571bf6ce69
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2025-09-24 13:22:15 +0100

    Dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,7 @@
-declare function get(key: string): Promise<any>;
+declare function get(key: string): Promise<string | null>;
 declare function set(key: string, value: string): Promise<void>;
 declare function exists(key: string): Promise<boolean>;
 declare function remove(key: string): Promise<void>;
+declare function keys(): Promise<string[]>;
 
-export { exists, get, remove, set };
+export { exists, get, keys, remove, set };
--- dist/index.js
+++ dist/index.js
@@ -32,6 +32,7 @@ var index_exports = {};
 __export(index_exports, {
   exists: () => exists,
   get: () => get,
+  keys: () => keys,
   remove: () => remove,
   set: () => set
 });
@@ -40,13 +41,17 @@ var fs = __toESM(require("fs/promises"));
 var cbor = __toESM(require("cbor"));
 var import_path = __toESM(require("path"));
 async function get(key) {
-  createDirectoryIfAbsent();
-  const raw = await fs.readFile(import_path.default.join(process.cwd(), `/.kv/${key}.cbor`));
-  const decoded = await cbor.decodeFirst(raw);
-  return decoded;
+  await createDirectoryIfAbsent();
+  try {
+    const raw = await fs.readFile(import_path.default.join(process.cwd(), `/.kv/${key}.cbor`));
+    const decoded = await cbor.decodeFirst(raw);
+    return decoded?.value?.toString();
+  } catch {
+    return null;
+  }
 }
 async function set(key, value) {
-  createDirectoryIfAbsent();
+  await createDirectoryIfAbsent();
   const encoded = await cbor.encodeAsync({ key, value });
   await fs.writeFile(import_path.default.join(process.cwd(), `/.kv/${key}.cbor`), encoded);
 }
@@ -61,6 +66,14 @@ async function exists(key) {
 async function remove(key) {
   await fs.unlink(import_path.default.join(process.cwd(), `/.kv/${key}.cbor`));
 }
+async function keys() {
+  try {
+    const files = await fs.readdir(import_path.default.join(process.cwd(), "/.kv"));
+    return files.map((file) => file.replace(".cbor", ""));
+  } catch {
+    return [];
+  }
+}
 async function createDirectoryIfAbsent() {
   const directory = import_path.default.join(process.cwd(), "/.kv");
   try {
@@ -73,6 +86,7 @@ async function createDirectoryIfAbsent() {
 0 && (module.exports = {
   exists,
   get,
+  keys,
   remove,
   set
 });
